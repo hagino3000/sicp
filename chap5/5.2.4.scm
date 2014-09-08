610a81bd0f64e2c5dcebb1cb3772bcb68dde081f
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2014-09-08 22:50:52 +0900

    Add q5.2

---
+++ chap5/5.2.4.scm
@@ -0,0 +1,24 @@
+(load "./register_machine.scm")
+
+(define gcd-machine
+ (make-machine
+  '(a b t)
+  (list (list 'rem remainder) (list '= =))
+  '(test-b
+      (test (op =) (reg b) (const 0))
+      (branch (label gcd-done))
+      (assign t (op rem) (reg a) (reg b))
+      (assign a (reg b))
+      (assign b (reg t))
+      (goto (label test-b))
+   gcd-done)))
+
+
+(set-register-contents! gcd-machine 'a 206)
+(set-register-contents! gcd-machine 'b 40)
+(start gcd-machine)
+(print (get-register-contents gcd-machine 'a))
+(print "======= gcd-machine =======")
+
+
+
