0b9e5f8e4692d8978c5d6453c6ad74c7e7dc9ac1
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2014-09-29 18:21:08 +0900

    Add q5.17

---
+++ chap5/q5.17.scm
@@ -0,0 +1,48 @@
+(load "./register_machine_with_trace2.scm")
+
+(define factorial-machine
+ (make-machine
+  '(n val continue)
+  (list
+   (list '= =)
+   (list '* *)
+   (list '- -))
+  '(
+   start
+      (assign continue (label fact-done))
+   fact-loop
+      (test (op =) (reg n) (const 1))
+      (branch (label base-case))
+      (save continue)
+      (save n)
+      (assign n (op -) (reg n) (const 1))
+      (assign continue (label after-fact))
+      (goto (label fact-loop))
+   after-fact
+      (restore n)
+      (restore continue)
+      (assign val (op *) (reg n) (reg val))
+      (goto (reg continue))
+   base-case
+      (assign val (const 1))
+      (goto (reg continue))
+   fact-done)))
+
+
+(define n 2)
+(set-register-contents! factorial-machine 'n n)
+(factorial-machine 'reset-instruction-count)
+(start factorial-machine)
+(print "n=>" n " val=>" (get-register-contents factorial-machine 'val))
+
+(factorial-machine 'trace-on)
+(set-register-contents! factorial-machine 'n n)
+(factorial-machine 'reset-instruction-count)
+(start factorial-machine)
+(print "n=>" n " val=>" (get-register-contents factorial-machine 'val))
+
+(factorial-machine 'trace-off)
+(set-register-contents! factorial-machine 'n n)
+(factorial-machine 'reset-instruction-count)
+(start factorial-machine)
+(print "n=>" n " val=>" (get-register-contents factorial-machine 'val))
